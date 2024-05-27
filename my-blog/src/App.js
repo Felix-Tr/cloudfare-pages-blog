10d3c0a2a89e82929556a45c7cb04424d57f8891
Felix <felix@treutwein/de>
2024-05-27 08:31:33 +0200

    added turtle and colours

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 import BlogPostList from './BlogPostList';
 import BlogPost from './BlogPost';
 import GraphVisualization from './GraphVisualization';
+import logo from './assets/images/android-chrome-512x512.png';
 
 function App() {
+  const [direction, setDirection] = useState('right');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDirection((prevDirection) => (prevDirection === 'right' ? 'left' : 'right'));
+    }, 20000); // Change direction every 20 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <header>
-          <h1>My Blog</h1>
+          <div className={`logo-container ${direction}`}>
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
           <nav>
             <ul>
               <li><Link to="/">Home</Link></li>
@@ -22,7 +35,7 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
             <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/podcasts" element={<Podcasts />} />
             <Route path="/about" element={<About />} />
@@ -30,7 +43,7 @@ function App() {
           </Routes>
         </main>
         <footer>
-          <p>&copy; 2023 My Blog. All rights reserved.</p>
+          <p>&copy; 2023 FT's Website. All rights reserved.</p>
         </footer>
       </div>
     </Router>
